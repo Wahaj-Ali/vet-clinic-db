883cf80a3a82043d753c8a38fbb2aa4c6b84d1bc
Wahaj Ali <wahaj.ali96@yahoo.com>
2023-02-21 18:36:08 +0500

    Update queries.sql
    In this commit
    - Add the queries to update the table using transaction and otherwise.

--- queries.sql
+++ queries.sql
@@ -8,3 +8,45 @@
  SELECT * FROM animals WHERE neutered IS TRUE;
  SELECT * FROM animals WHERE name!='Gabumon';
  SELECT name,escape_attempts FROM animals WHERE weight_kg>10.5;
+
+BEGIN;
+UPDATE animals
+SET
+species = 'unspecified'
+WHERE
+id>0;
+ROLLBACK;
+
+BEGIN;
+UPDATE animals
+SET
+species='digimon'
+WHERE
+name LIKE '%mon%';
+UPDATE animals
+SET
+species='pokemon'
+WHERE
+species IS NULL;
+COMMIT;
+SELECT * FROM animals; /*To verify the changes*/
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+SELECT * FROM animals; /*To verify the changes*/
+
+BEGIN;
+DELETE FROM animals
+WHERE date_of_birth>'2022-01-01';
+SAVEPOINT SP1;
+UPDATE animals
+SET
+weight_kg = weight_kg*-1;
+ROLLBACK TO SP1;
+UPDATE animals
+SET
+weight_kg = weight_kg*-1
+WHERE
+weight_kg<0;
+COMMIT;
